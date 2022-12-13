bc36537aff7e1d74aea77a4b92d1da9a9571bc52
LINGARAJ <lingaraj01052002@gmail.com>
2022-12-13 17:03:05 +0530

    update.

---
+++ main.js
@@ -0,0 +1,35 @@
+$('.events-row').owlCarousel({
+    loop:true,
+    margin:10,
+	autoplay:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.review-row').owlCarousel({
+    loop:true,
+    margin:10,
+	autoplay:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
